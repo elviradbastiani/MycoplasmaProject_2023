a0dc590a3eb6a6a3da5eadc7aea197d6ea84886c
Elvira D'Bastiani <elviradbastiani@gmail.com>
2024-01-29 17:46:36 -0800

    Update Script_2_Analysing_networks.R

--- code/Script_2_Analysing_networks.R
+++ code/Script_2_Analysing_networks.R
@@ -2,17 +2,17 @@
 # Manuscript: "Social interactions do not  impact infection status in griffon vultures"
 # Elvira D'Bastiani1*, Nili Anglister2, Inna Lysynyansky3, Inna Mikula3, Marta Acácio2,
 # Gideon Vaadia2, Kaija Gahm1, Orr Spiegel2, Noa Pinter-Wollman1
-# 1.Department of Ecology and Evolutionary Biology, University of California Los Angeles, 
+# 1. Department of Ecology and Evolutionary Biology, University of California Los Angeles, 
 # Los Angeles, California, USA
-# 2.School of Zoology, Faculty of Life Sciences, Tel Aviv University, Tel Aviv, Israel.
-# 3.Department of Avian Diseases, Professor Kimron Str 1, Kimron Veterinary Institute (KVI),
+# 2. School of Zoology, Faculty of Life Sciences, Tel Aviv University, Tel Aviv, Israel.
+# 3. Mycoplasma unit, Department of Avian Diseases, Professor Kimron Str 1, Kimron Veterinary Institute (KVI),
 # POB12, Beit Dagan, Israel.
 # *Corresponding author: Elvira D'Bastiani e-mail: elviradbastiani@gmail.com
 #*****************************************************************************************
 
 ###############################################################################################################################
 ###############################################################################################################################
-#Script to analysing the social networks of griffon vultures
+#Script to analyzing the social networks of griffon vultures
 ###############################################################################################################################
 ###############################################################################################################################
 
