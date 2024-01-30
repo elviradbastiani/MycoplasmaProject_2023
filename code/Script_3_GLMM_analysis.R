b8d81bed1a487d491203457aaaa0acff4db7c8de
Elvira D'Bastiani <elviradbastiani@gmail.com>
2024-01-30 09:21:13 -0800

    Update Script_3_GLMM_analysis.R

--- code/Script_3_GLMM_analysis.R
+++ code/Script_3_GLMM_analysis.R
@@ -12,7 +12,6 @@
 
 Citation: D’Bastiani, E.*, Anglister, N., Lysynyansky, I., Mikula, I., Acácio, M., Vaadia, G., Gahm, K., Spiegel, O., Pinter-Wollman, N. "Social Interactions and Mycoplasma Infection in Griffon Vultures." Journal of [Journal Name], [Year], [Volume(Issue)], [Page Range]. DOI: [DOI Number].
 
-
 ###############################################################################################################################
 ###############################################################################################################################
 #Script to analyzing the relationship between mycoplasma infection status and social position (degree, betweenness, and strength) 
